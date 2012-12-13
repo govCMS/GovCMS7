641cb17ddd1dc112b92a9de1ce530c1bbe6d5b69
Nick Schuch <nick@previousnext.com.au>
2012-12-14 09:56:31 +1000

    Update logic of front page panels layout.

--- modules/custom/agov_layouts/plugins/layouts/front/agov-layouts-front.tpl.php
+++ modules/custom/agov_layouts/plugins/layouts/front/agov-layouts-front.tpl.php
@@ -17,20 +17,19 @@
 <div class="gov-front-layout clearfix" <?php if (!empty($css_id)) : print "id=\"$css_id\""; endif; ?>>
   
   <?php if (!empty($content['main'])) : ?>
-      <?php print $content['main'];?>
+    <?php print $content['main'];?>
+  <?php endif; ?>
 
-      <?php if (!empty($content['left'])) : ?>
-        <div class="alpha grid-4 gov-front-left gov-front-col">
-          <?php print $content['left'];?>
-        </div>
-      <?php endif; ?>
+  <?php if (!empty($content['left'])) : ?>
+    <div class="alpha grid-4 gov-front-left gov-front-col">
+      <?php print $content['left'];?>
+    </div>
+  <?php endif; ?>
 
-      <?php if (!empty($content['right'])) : ?>
-        <div class="omega grid-4 gov-front-right gov-front-col">
-          <?php print $content['right'];?>
-        </div>
-      <?php endif; ?>
-
+  <?php if (!empty($content['right'])) : ?>
+    <div class="omega grid-4 gov-front-right gov-front-col">
+      <?php print $content['right'];?>
+    </div>
   <?php endif; ?>
 
 </div>
