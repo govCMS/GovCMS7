5f061c0db4bab2fa9541c78783de1e30ea370437
jptaranto <jacktaranto@gmail.com>
2013-01-18 16:45:08 +1100

    [HOTFIX] Changed all sass output to expanded style.

--- themes/agov_base/config.rb
+++ themes/agov_base/config.rb
@@ -8,7 +8,7 @@ images_dir = "images"
 javascripts_dir = "js"
 fonts_dir = "fonts"
 
-output_style = :nested
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
