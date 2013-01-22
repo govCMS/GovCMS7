b863279dcd1034784a8afb3b811b27796a8c3a12
Joshua Russell <joshuarussell@me.com>
2013-01-22 15:48:15 +1100

    [#11475] Updated link text and added a title to section for screen readers

--- modules/custom/agov_text_resize/theme/text-resize.tpl.php
+++ modules/custom/agov_text_resize/theme/text-resize.tpl.php
@@ -8,20 +8,21 @@
 ?>
 
 <div id="resize-buttons">
+  <span class="element-invisible">Text size</span>
 	<ul>
 		<li class="font-small">
 			<a href="#">
-		    <?php print t('Smaller'); ?>
+		    <?php print t('Smaller text'); ?>
 			</a>
 		</li>
 		<li class="font-large">
 			<a href="#">
-				<?php print t('Larger'); ?>
+				<?php print t('Larger text'); ?>
 			</a>
 		</li>
 		<li class="reset">
 			<a href="#">
-				<?php print t('Reset'); ?>
+				<?php print t('Reset text size'); ?>
 			</a>
 		</li>
 	</ul>
