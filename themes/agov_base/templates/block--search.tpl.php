44a1fd0cf373c381dc82564ead09b28f5ac2c120
Kim Pepper <kim@pepper.id.au>
2013-03-08 10:04:05 +1100

    [#12153] Attribute should be 'role' not 'aria-role'

--- themes/agov_base/templates/block--search.tpl.php
+++ themes/agov_base/templates/block--search.tpl.php
@@ -1,7 +1,7 @@
 <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
   <span class="element-invisible">Search Content</span>
-  <div aria-role="search">
+  <div role="search">
     <?php print $content ?>
   </div>
 
