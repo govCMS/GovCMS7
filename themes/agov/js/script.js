88ac31a2e01c5d1886e95b5aef1ef1e983441eb3
Robert Caracaus <rcaracaus@gmail.com>
2013-11-26 19:42:59 +1100

    Added Zen and agov Zen-subtheme -- set it as default with install profile

---
+++ themes/agov/js/script.js
@@ -0,0 +1,37 @@
+/**
+ * @file
+ * A JavaScript file for the theme.
+ *
+ * In order for this JavaScript to be loaded on pages, see the instructions in
+ * the README.txt next to this file.
+ */
+
+// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - https://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+(function ($, Drupal, window, document, undefined) {
+
+
+// To understand behaviors, see https://drupal.org/node/756722#behaviors
+Drupal.behaviors.mainMenuTinyNav = {
+  attach: function(context, settings) {
+    $(".region-navigation .block__content > .menu").tinyNav();
+  }
+};
+
+Drupal.behaviors.mainMenuSuperfish = {
+  attach: function(context, settings) {
+	  
+	  var superfish_menu = $(".region-navigation .block__content > .menu");
+	  
+		superfish_menu.addClass('sf-menu');
+    superfish_menu.superfish({ 
+			autoArrows:  false                           
+		});
+
+		
+  }
+};
+
+})(jQuery, Drupal, this, this.document);
