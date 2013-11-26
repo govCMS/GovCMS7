858b499195e8b5dd8555d82ea4a784ab8ea3f9bc
Robert Caracaus <rcaracaus@gmail.com>
2013-11-26 19:33:08 +1100

    Drupal.org version needed to work with Drush SI

--- themes/agov_base/config.rb
+++ themes/agov_base/config.rb
@@ -2,21 +2,12 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-
-# Directories
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "js"
 fonts_dir = "fonts"
-
-# Other options
 relative_assets = true
-line_comments = false
-color_output = false
-
-# Don't add cache-busing hash's to generated image urls
-asset_cache_buster :none
 
 # Leave this as expanded for now.
 output_style = :expanded
@@ -25,12 +16,20 @@ output_style = :expanded
 # Be sure to re-comment it before commiting.
 # sass_options = {:debug_info => true}
 
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+color_output = false
+
+
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
 preferred_syntax = :scss
+
 
 # Require any additional compass plugins installed on your system.
 require 'breakpoint'
