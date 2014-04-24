662a93731cd250efeb14d25a1f48293649e06634
Chris Skene <chris@xtfer.com>
2014-04-24 16:23:51 +1000

    [#22205, #22206] Add aGov STARTERKIT for building agov_zen sub themes.

---
+++ themes/AGOV_STARTERKIT/theme-settings.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @file
+ * Theme settings.
+ */
+
+/**
+ * Implements hook_form_system_theme_settings_alter().
+ */
+function AGOV_STARTERKIT_form_system_theme_settings_alter(&$form, &$form_state, $form_id = NULL)  {
+
+  // Work-around for a core bug affecting admin themes. See issue #943212.
+  if (isset($form_id)) {
+    return;
+  }
+
+  // Create the form using Forms API: http://api.drupal.org/api/7
+  /* -- Delete this line if you want to use this setting
+  $form['AGOV_STARTERKIT_example'] = array(
+    '#type'          => 'checkbox',
+    '#title'         => t('AGOV_STARTERKIT sample setting'),
+    '#default_value' => theme_get_setting('AGOV_STARTERKIT_example'),
+    '#description'   => t("This option doesn't do anything; it's just an example."),
+  );
+  // */
+
+  // Remove some of the base theme's settings.
+  /* -- Delete this line if you want to turn off this setting.
+  // We don't need to toggle wireframes on this site.
+  unset($form['themedev']['zen_wireframes']);
+  // */
+
+  // We are editing the $form in place, so we don't need to return anything.
+}
