b77e60ab4d786171faf1c4731ffa0f6c278864ab
Kim Pepper <kim@pepper.id.au>
2014-11-12 13:39:35 +1100

    [#22580] Replaces * with (requried)

--- themes/agov/agov_zen/template.php
+++ themes/agov/agov_zen/template.php
@@ -98,3 +98,16 @@ function agov_zen_status_messages($variables) {
   }
   return $output;
 }
+
+/**
+ * Implements theme_form_required_marker().
+ */
+function agov_zen_form_required_marker($variables) {
+  // This is also used in the installer, pre-database setup.
+  $t = get_t();
+  $attributes = array(
+    'class' => 'form-required',
+    'title' => $t('This field is required.'),
+  );
+  return '<span' . drupal_attributes($attributes) . '>(required)</span>';
+}
