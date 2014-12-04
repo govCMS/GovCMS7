0b30145ddcfd1e691d48ce1e90de2e60da7cdf45
Nick Schuch <nick@myschuch.com>
2014-12-04 13:48:43 +1000

    Adds Vagrant.

---
+++ puppet/custom/drupal/manifests/params.pp
@@ -0,0 +1,14 @@
+# Drupal common parameters.
+
+class drupal::params (
+  $port                = '80',
+  $display_errors      = 'false',
+  $memory_limit        = '256M',
+  $post_max_size       = '20M',
+  $upload_max_filesize = '20M',
+  $max_execution_time  = '300',
+  $date_timezone       = 'Australia/Sydney',
+  $error_log           = 'syslog',
+){
+  # Nothing to see here.
+}
