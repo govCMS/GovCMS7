b878f450136914ec744a98e0fab74cd0ce933815
James Gollan <james@swamphen.com>
2015-04-16 11:46:53 +1000

    Remove unused footer variable.

--- themes/govcms/govcms_zen/template.php
+++ themes/govcms/govcms_zen/template.php
@@ -44,8 +44,6 @@ function govcms_zen_preprocess_maintenance_page(&$variables) {
 
   $t_function = get_t();
 
-  $variables['footer'] = '';
-
   if (drupal_installation_attempted()) {
     $variables['logo'] = base_path() . drupal_get_path('theme', 'govcms_zen') . '/logo-alt.png';
     // Override the site name, which will be "Drupal".
