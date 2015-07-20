6d9edf1d96a8d9880ff35d24010151d3cde73dba
Stuart Rowlands <stuart.rowlands@acquia.com>
2015-07-20 13:56:49 +1000

    Make behat tests phantomjs compatible.

--- build/tests/behat/bootstrap/FeatureContext.php
+++ build/tests/behat/bootstrap/FeatureContext.php
@@ -123,17 +123,41 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
   }
 
   /**
+   * Sets an id for the first iframe situated in the element specified by id.
+   * Needed when wanting to fill in WYSIWYG editor situated in an iframe without identifier.
+   *
+   * @Given /^the iframe in element "(?P<element>[^"]*)" has id "(?P<id>[^"]*)"$/
+   */
+  public function theIframeInElementHasId($element_id, $iframe_id) {
+    $function = <<<JS
+(function(){
+  var elem = document.getElementById("$element_id");
+  var iframes = elem.getElementsByTagName('iframe');
+  var f = iframes[0];
+  f.id = "$iframe_id";
+})()
+JS;
+    try {
+      $this->getSession()->executeScript($function);
+    }
+    catch(Exception $e) {
+      throw new \Exception(sprintf('No iframe found in the element "%s" on the page "%s".', $element_id, $this->getSession()->getCurrentUrl()));
+    }
+  }
+
+  /**
    * Fills in WYSIWYG editor with specified id.
    *
-   * @Given /^(?:|I )enter "(?P<text>[^"]*)" for WYSIWYG "(?P<iframe>[^"]*)"$/
+   * @Given /^(?:|I )fill in "(?P<text>[^"]*)" in WYSIWYG editor "(?P<iframe>[^"]*)"$/
    */
   public function iFillInInWYSIWYGEditor($text, $iframe) {
     try {
       $this->getSession()->switchToIFrame($iframe);
-    } catch (Exception $e) {
-      throw new \Exception(sprintf("No iframe with id '%s' found on the page '%s'.", $iframe, $this->getSession()->getCurrentUrl()));
     }
-    $this->getSession()->executeScript("document.body.innerHTML = '<p>" . $text . "</p>'");
+    catch (Exception $e) {
+      throw new \Exception(sprintf("No iframe with id '%s' found on the page '%s'.", $iframe, $this->getSession()->getCurrentUrl()));
+    } 
+    $this->getSession()->executeScript("document.body.innerHTML = '<p>".$text."</p>'");      
     $this->getSession()->switchToIFrame();
   }
 
