59f2c2723e58c18a6a03304597c260c446a0265f
Stuart Rowlands <stuart.rowlands@acquia.com>
2015-12-31 20:22:25 +1000

    Add govCMS Consultation (Have Your Say) feature.

---
+++ modules/features/govcms_consultation/templates/paragraphs-item--update-milestone.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * Channels theme implementation for a container.
+ */
+?>
+<div class="update">
+  <div class="update__icon"><?php print render($content['field_update_type_term']); ?></div>
+  <div class="update__title"><?php print render($content['field_pbundle_destination']); ?></div>
+  <div class="update__date"><?php print render($content['field_update_date']); ?></div>
+</div>
