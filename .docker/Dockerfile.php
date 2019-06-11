31c89755bfc0434d21f620627025aa83c1c7400a
Steve Worley <sj.worley88@gmail.com>
2019-06-11 11:42:51 +1000

    Remove GMP from the govCMS build.
    GMP is only required for d7 if the openid module is being used.
    
    - Removes GMP from cli image.
    - Removes GMP from php image.

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -3,8 +3,4 @@ FROM ${CLI_IMAGE} as cli
 
 FROM amazeeio/php:7.1-fpm
 
-RUN apk add gmp gmp-dev \
-    && docker-php-ext-install gmp \
-    && docker-php-ext-configure gmp
-
 COPY --from=cli /app /app
